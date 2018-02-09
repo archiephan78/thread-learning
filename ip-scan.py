92758a2d07fc0d4ca2376be845a23e1d3142a6d6
Phan Huu Thanh Chung <chungpht@vccloud.vn>
2018-02-09 16:22:51 +0700

    update queue

--- ip-scan.py
+++ ip-scan.py
@@ -3,7 +3,10 @@ import ipaddress
 import threading
 import os
 import subprocess
+from multiprocessing import Queue
 
+queue = Queue()
+threads = [ ]
 net_addr = raw_input ("Nhap network duoi dang CIDR format(192.168.1.0/24): ")
 net = unicode(net_addr, "utf-8")
 my_net = ipaddress.ip_network(net)
@@ -12,16 +15,17 @@ list_hosts = list(my_net.hosts())
 
 def check_ping(list_hosts, i):
   reponse = subprocess.call([ 'ping', '-c', '1', str(list_hosts[i]) ],stdout=subprocess.PIPE)
+  queue.put(reponse)
   if reponse == 0:
     print( str(list_hosts[i]) + " " "Online")
   else:
     print( str(list_hosts[i]) + " " "Offline")
-#
-def main():
-  for i in range (len(list_hosts)):
-    thread = threading.Thread(target=check_ping, args=(list_hosts, i,  ))
-    thread.start()
+##
 
 if __name__ == '__main__':
-  main()
+  for i in range (len(list_hosts)):
+    t = threading.Thread(target=check_ping, args=(list_hosts, i,  ))
+    threads.append(t)
+    t.start()
+
 
